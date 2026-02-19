8ada5eb7f9888c7848000d4f59ce6715b37dbe61
Syed Mustafa Muzaffar <syednabimuzammilicloud.com@Syeds-MacBook-Air.local>
2026-02-19 16:52:23 +0530

    Fix Vercel 404 and update API URL

--- src/axios.js
+++ src/axios.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const instance = axios.create({
-    baseURL: "http://localhost:5001/api",
+    baseURL: import.meta.env.PROD ? "/api" : "http://localhost:5001/api",
 });
 
 export default instance;
